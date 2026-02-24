c2fd15fbd0b1c91808e13c12f492bb1591a9df67
Vaibhav <rawatvaibhav42@gmail.com>
2026-02-24 16:18:24 +0530

    fixed toast layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <App />
       <Toaster
-        position="top-right"
+        position="top-center"
         toastOptions={{
           duration: 4000,
           style: {
